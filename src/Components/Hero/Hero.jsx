19c976380a7f3d45e59fc7eb4882df744ade5e15
amit kumar <mohapatraamitkumar450@gmail.com>
2024-01-10 23:03:48 +0530

    hero is added to shop

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -3,11 +3,12 @@ import './Hero.css'
 
 import hand_icon from "../Assets/hand_icon.png"
 import arrow_icon from "../Assets/arrow.png"
+import hero_img from "../Assets/hero_image.png"
 
 const Hero = () => {
   return (
     <div className='hero'>
-        <div className="her-left">
+        <div className="hero-left">
             <h2>NEW ARRIVALS ONLY</h2>
             <div>
                 <div className="hero-hand-icon">
@@ -22,7 +23,9 @@ const Hero = () => {
                 <img src={arrow_icon} alt="" />
             </div>
         </div>
-        <div className="hero-right"></div>
+        <div className="hero-right">
+            <img src={hero_img} alt="hero_img" />
+        </div>
 
     </div>
   )
