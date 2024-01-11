fa8f28ae14afdb9c03940853e632f52e485837a2
amit kumar <mohapatraamitkumar450@gmail.com>
2024-01-11 23:07:41 +0530

    Offers component is added

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from '../Components/Hero/Hero'
 import Popular from '../Components/Popular/Popular'
+import Offers from '../Components/Offers/Offers'
 
 function Shop() {
   return (
     <div style={{display: 'flex', flexDirection: "column", justifyContent: "space-between"}}>
         <Hero />
         <Popular />
+        <Offers />
     </div>
   )
 }
